dd82a7a4fea6e8495cc42a0b9ecdde9f78fdf92a
Fernandoper92 <fernandoper92@gmail.com>
2020-11-26 12:39:07 -0300

    commit

--- script.js
+++ script.js
@@ -21,7 +21,8 @@ function readJson(value) {
 
 function onSubmit(event) {
     event.preventDefault();
-    const value = document.getElementById('input-value').value;
+    let value = document.getElementById('input-value').value;
+    value = value.toUpperCase();
     checkValue(value);
 
 }
@@ -38,33 +39,39 @@ function compareData(result, value) {
     const encomendas = result.encomendas;
     for (i in encomendas) {
         numero = encomendas[i].numero;
-        if(value == numero) {
-           return showData(encomendas[i]);
+        if (value == numero) {
+            return showData(encomendas[i]);
         } else {
             showError();
         }
-        
+
     }
 }
 
 function showData(data) {
     let success = document.querySelector('.success-grid-container');
     let error = document.querySelector('.error-flex-container');
-    success.setAttribute('style','display:grid')
-    error.setAttribute('style','display:none')
+    let price = data.valor;
+    price = price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+    let date = data.data;
+    date = date.split('T');
+    date = date[0].split('-');
+    date = date[2] + '/' + date[1] + '/' + date[0];
+    success.setAttribute('style', 'display:grid')
+    error.setAttribute('style', 'display:none')
 
-    const cliente = document.querySelector('.client-value').innerHTML = data.cliente.id + " - " + data.cliente.nome;
-    const price = document.querySelector('.price-value').innerHTML = "R$ " + data.valor + ",00";
-    const date = document.querySelector('.date-value').innerHTML = data.data;
-   
-    if(data.entregue) {const status = document.querySelector('.status-value').innerHTML = "Entregue"};
-    if(!data.entregue) {const status = document.querySelector('.status-value').innerHTML = "Entregar"};
+    document.querySelector('.client-value').innerHTML = data.cliente.id + " - " + data.cliente.nome;
+    document.querySelector('.price-value').innerHTML = price;
+    document.querySelector('.date-value').innerHTML = date;
+
+    if (data.entregue) { const status = document.querySelector('.status-value').innerHTML = "Entregue" };
+    if (!data.entregue) { const status = document.querySelector('.status-value').innerHTML = "Entregar" };
 
 }
 
 function showError() {
     let success = document.querySelector('.success-grid-container');
     let error = document.querySelector('.error-flex-container');
-    success.setAttribute('style','display:none')
-    error.setAttribute('style','display:flex')
+    success.setAttribute('style', 'display:none')
+    error.setAttribute('style', 'display:flex')
 }
